2449034a6554fc3794b9b0a2436734e7c66e72ba
Naveed Usmani <nu@blue-ocean-robotics.com>
2018-12-18 15:33:02 +0100

    comments out and adds a not to CATCH_CONFIG_MAIN macro

--- test/catch_test.cpp
+++ test/catch_test.cpp
@@ -1,4 +1,6 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+// NOTE: not to be done when using with catch_ros cmake macros
+// #define CATCH_CONFIG_MAIN
+
 #include <catch_ros/catch.hpp>
 
 TEST_CASE("test_case", "[some tag]")
